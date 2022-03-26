02214f935b1a6128119b81eb0e1877ad7c2cb6c8
Helen Trapeznikova <strelchenya-91@mail.ru>
2022-03-26 20:10:33 +0300

    added responsive

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+
+window.ontouchstart = function(event){
+    if(event.target == close){
+        menu.classList.add('header__menu--close');
+    }
+    if(event.target == open){
+        menu.classList.remove('header__menu--close');
+    }
+}
+
